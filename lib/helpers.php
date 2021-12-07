eb7711d1e9fcb5c8b6f13d5b62ed68c0301a89f4
frageno <szymon.piatkowski27@gmail.com>
2021-12-07 20:38:11 +0100

    Added new function which allows translate strings in the theme

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,21 +1,41 @@
 <?php
 
 function firsttheme_post_meta(){
-    
-        echo 'Posted on:';
-        echo '<a href="'.  esc_url(get_permalink()) . '">';
-        echo '<time datetime="' . esc_attr(get_the_date('c')) .'">' .  esc_html(get_the_date()) .  '</time>';
-        echo '</a>';
-        echo 'By:';
-        echo '<a href="' . esc_url(get_author_posts_url( get_the_author_meta('ID'))). '">'. esc_html(get_the_author()) .
-        '</a>';
-       
+
+        /* translators: %s is Post Date*/
+        printf(
+            esc_html__('Posted on: %s', 'firsttheme'),
+            '<a href="'.  esc_url(get_permalink()) . '"><time datetime="' .
+            esc_attr(get_the_date('c')) .'">' .  esc_html(get_the_date()) .  '</time></a>'
+        );
+
+        /* translators: %s is Post Author */
+
+        printf(
+            esc_html__(' By: %s', 'firsttheme'),
+            '<a href="' . esc_url(get_author_posts_url( get_the_author_meta('ID'))). '">' .esc_html(get_the_author()) .
+            '</a>'
+        );
+     
       
 }
 
 
 function firsttheme_read_more(){
     echo '<a href="' . esc_url(get_the_permalink()). '" title="' . the_title_attribute(['echo' => false])   . '">';
-    echo  'Read More <span class="u-screen-reader-text"> About ' .  get_the_title() .  '</span>';
+
+    /* translators: %s is Post Title */
+    printf(
+        wp_kses(
+            __('Read More <span class="u-screen-reader-text"> About %s </span>'), 
+            [
+                'span' => 
+                [
+                    'class' => ""
+                ]
+            ]
+        ),
+        get_the_title()
+    );
     echo  '</a>';
 }
